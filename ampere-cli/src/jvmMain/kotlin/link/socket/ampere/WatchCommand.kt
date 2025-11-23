3ce0e0a4468b4573df1a8521b54388db7ba91878
Miley Chandonnet <email@miley.codes>
2025-11-22 22:43:52 -0500

    Fix typo

--- ampere-cli/src/jvmMain/kotlin/link/socket/ampere/WatchCommand.kt
+++ ampere-cli/src/jvmMain/kotlin/link/socket/ampere/WatchCommand.kt
@@ -4,8 +4,15 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.int
+<<<<<<< Updated upstream
 import com.github.ajalt.mordant.rendering.TextColors.*
 import com.github.ajalt.mordant.rendering.TextStyles.*
+=======
+import com.github.ajalt.mordant.rendering.TextColors.cyan
+import com.github.ajalt.mordant.rendering.TextColors.yellow
+import com.github.ajalt.mordant.rendering.TextStyles.bold
+import com.github.ajalt.mordant.rendering.TextStyles.dim
+>>>>>>> Stashed changes
 import com.github.ajalt.mordant.terminal.Terminal
 
 class WatchCommand : CliktCommand(
@@ -51,7 +58,7 @@ class WatchCommand : CliktCommand(
 
     override fun run() {
         terminal.println(
-            bold(cyan("AMPERE")) + " - AniMA Perception & Relay Environment"
+            bold(cyan("AMPERE")) + " - AniMA Model Protocol Example Runtime Environment"
         )
         terminal.println(dim("Connecting to event stream..."))
         terminal.println()
