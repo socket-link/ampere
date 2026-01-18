ae17692e10e4b40615ed0ecebfc73e8879a4580d
Miley Chandonnet <email@miley.codes>
2026-01-17 23:20:35 -0500

    Clarify Jazz demo task-create copy

---
+++ ampere-cli/src/jvmTest/kotlin/link/socket/ampere/JazzDemoCommandTest.kt
@@ -0,0 +1,17 @@
+package link.socket.ampere
+
+import com.github.ajalt.clikt.testing.test
+import kotlin.test.Test
+import kotlin.test.assertContains
+
+class JazzDemoCommandTest {
+
+    @Test
+    fun `jazz demo help text mentions task create command`() {
+        val command = JazzDemoCommand { error("context should not be needed for help output") }
+        val result = command.test("--help")
+
+        assertContains(result.output, "Jazz Test demo")
+        assertContains(result.output, "ampere task create")
+    }
+}
